8dc269caf4c9d6c409bef1d5829a35a67bc536d8
EMQ-YangM <yangm@emqx.io>
2021-11-17 16:34:20 +0800

    binary

--- helloworld.erl
+++ helloworld.erl
@@ -55,6 +55,7 @@ start() ->
     io:fwrite("~w~n", [?macrol(1, 2)]),
     io:fwrite("~w~n", [time()]),
     io:fwrite("~w~n", [tuple_to_list({1, 2, 3})]),
+    io:fwrite("~w~n", [<<"hello">>]),
     io:fwrite("finish\n").
 
 for(0, _) ->
