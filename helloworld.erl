f5867b7315837d9dbe2a37d40fef9ea524a5ecf9
EMQ-YangM <yangm@emqx.io>
2021-11-17 15:04:53 +0800

    function

--- helloworld.erl
+++ helloworld.erl
@@ -16,10 +16,18 @@ while([_ | T], Acc) ->
 start() ->
     X = [1, 2, 3, 4],
     while(X),
-    for(5, atom).
+    for(5, atom),
+    Fn = fun() ->
+       io:fwrite("some function\n") end,
+    Fn(),
+    add(10,20).
 
 for(0, _) ->
     [];
 for(N, Term) when n > 0 ->
     io:fwrite("atom"),
     [Term | for(N - 1, Term)].
+
+add(X,Y) ->
+    Z = X + Y,
+    io:fwrite("~w~n", [Z]).
